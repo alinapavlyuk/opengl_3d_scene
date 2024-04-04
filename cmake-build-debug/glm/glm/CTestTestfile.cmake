2c5471c0f52e6f174827bc7f1db6d69bf571bc9b
alinapavlyuk <43186632+alinapavlyuk@users.noreply.github.com>
2024-04-04 23:01:20 +0900

    Delete cmake-build-debug directory

--- cmake-build-debug/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/apvlk/Desktop/kpi/графіка/lab2/glm/glm
-# Build directory: /Users/apvlk/Desktop/kpi/графіка/lab2/cmake-build-debug/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
